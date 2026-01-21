3eb0526f87f87f90066934867a6b62dabb064ac5
Dguminski12 <davidguminski12@gmail.com>
2026-01-21 17:22:12 +0000

    testing script to check password strength based on 5 checks -broken

---
+++ password-checker.py
@@ -0,0 +1,34 @@
+password = input("Enter your password: ")
+
+weak_password = 2
+medium_password = 3
+strong_password = 4
+
+checks_passed = 0
+
+
+def check_password_strength(pw):
+    if len(pw) > 8:
+        checks_passed += 1
+    if any(char.isdigit() for char in pw):
+        checks_passed += 1
+    if any(char.isupper() for char in pw):
+        checks_passed += 1
+    if any(char.islower() for char in pw):
+        checks_passed += 1
+    if any(char in "!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~" for char in pw):
+        checks_passed += 1
+    return checks_passed
+
+
+def evaluate_strength(checks):
+    if checks >= strong_password:
+        return "Strong password"
+    elif checks >= medium_password:
+        return "Medium password"
+    else:
+        return "Weak password"
+
+checks = check_password_strength(password)
+strength = evaluate_strength(checks) 
+print(f"Your password is: {strength}")
